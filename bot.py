b011a088eacc758df67a5e57a7460b2e23c0184d
Wizard <codenexusbotz@gmail.com>
2026-09-04 20:02:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,57 @@
+from datetime import datetime
+
 from pyrogram import Client
+from pyrogram.enums import ParseMode
 from pyromod import listen
-from pyrogram.enums import PharseMode
 
-class = Bot(Client):
+from config import (
+    API_ID,
+    API_HASH,
+    TG_BOT_TOKEN,
+    TG_BOT_WORKERS,
+    OWNER_ID,
+    LOGGER
+)
+
+
+class Bot(Client):
     def __init__(self):
-      super().__init__(
-          name="Bot",
-          api_id=API_ID,
-          api_hash=API_HASH,
-          plugins={
-            "root"= "plugins"
-          },
-          workers=TG_BOT_WORKERS,
-          bot_token=TG_BOT_TOKEN
-      )
-      self.LOGGER = LOGGER
+        super().__init__(
+            name="Bot",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            plugins={
+                "root": "plugins"
+            },
+            workers=TG_BOT_WORKERS,
+            bot_token=TG_BOT_TOKEN
+        )
 
-async des start(self):
-    await super().start()
+        self.LOGGER = LOGGER
+
+    async def start(self):
+        await super().start()
+
         usr_bot_me = await self.get_me()
         self.uptime = datetime.now()
 
-self.set_parse_mode(ParseMode.HTML)
-self.LOGGER(__name__).info("Bot is Started @{self.username}")
+        self.set_parse_mode(ParseMode.HTML)
 
- try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @PyroSznpai</blockquote></b>")
-        except: pass
+        self.LOGGER(__name__).info(
+            f"Bot is Started @{usr_bot_me.username}"
+        )
+
+        try:
+            await self.send_message(
+                OWNER_ID,
+                text=(
+                    "<b><blockquote>"
+                    "Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @PyroSznpai"
+                    "</blockquote></b>"
+                )
+            )
+        except Exception:
+            pass
 
     async def stop(self, *args):
         await super().stop()
